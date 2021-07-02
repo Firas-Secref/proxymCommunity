bfa93732c8fd50194ec48e54621b21c65c54d9c3
firas secref <sh.firas10@gmail.com>
2021-07-02 14:56:18 +0100

    first commit in this project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {ProfileComponent} from "./components/profile/profile.component";
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+// import {MatToolbarModule} from '@angular/material/toolbar';
+// import {MatSidenavModule} from '@angular/material/sidenav';
+// import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    // NgbModule,
+    // MatToolbarModule,
+    // MatSidenavModule,
+    // MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
